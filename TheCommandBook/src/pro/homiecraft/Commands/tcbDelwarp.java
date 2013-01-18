f1d5b748a2bac384689b6249b6febd7a8cbc67f8
Stian T. Home <stitofte@yahoo.no>
2013-01-18 22:29:48 +0100

    Added delwarp command. And added if warp exists on when their trying to warp somewhere.

--- TheCommandBook/src/pro/homiecraft/Commands/tcbDelwarp.java
+++ TheCommandBook/src/pro/homiecraft/Commands/tcbDelwarp.java
@@ -1,5 +1,40 @@
 package pro.homiecraft.Commands;
 
-public class tcbDelwarp {
+import java.io.File;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import pro.homiecraft.TheCommandBook;
+
+public class tcbDelwarp {
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (sender.hasPermission("tcb.delwarp")) {
+			if (cmd.getName().equalsIgnoreCase("delwarp")) {
+				if (args.length < 1){
+					sender.sendMessage("Missing warp name!");
+					sender.sendMessage("usage: /delwarp warpName");
+					return true;
+				}else if(args.length > 1){
+					sender.sendMessage("To many arguments!");
+					sender.sendMessage("usage: /delwarp warpName");
+					return true;
+				}else{
+					Player player = (Player) sender;
+					
+					File warpFile = new File(TheCommandBook.pluginST.getDataFolder() + "/data/warps/" + args[0] + ".yml");
+					if (warpFile.exists()){
+						warpFile.delete();						
+						player.sendMessage("Warp: " + args[0] + " deleted.");
+						return true;
+					}else{
+						sender.sendMessage("Warp " + args[0] + " does not exists!");
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
--- TheCommandBook/src/pro/homiecraft/Commands/tcbWarp.java
+++ TheCommandBook/src/pro/homiecraft/Commands/tcbWarp.java
@@ -1,5 +1,7 @@
 package pro.homiecraft.Commands;
 
+import java.io.File;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -7,6 +9,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import pro.homiecraft.TheCommandBook;
 import pro.homiecraft.Config.warpConfig;
 
 public class tcbWarp implements CommandExecutor {
@@ -26,17 +29,23 @@ public class tcbWarp implements CommandExecutor {
 					
 					String cworld = player.getWorld().getName();
 					
-					warpConfig.reloadWarpConfig(args[0]);
-					
-					int x = warpConfig.getWarpConfig(args[0]).getInt(args[0] + "." + cworld + ".X");
-					int y = warpConfig.getWarpConfig(args[0]).getInt(args[0] + "." + cworld + ".Y");
-					int z = warpConfig.getWarpConfig(args[0]).getInt(args[0] + "." + cworld + ".Z");
-					
-					Location warp = new Location(Bukkit.getWorld(cworld), x, y, z);
-					player.teleport(warp);
-					
-					player.sendMessage("Warping to: " + args[0]);
-					return true;
+					File warpFile = new File(TheCommandBook.pluginST.getDataFolder() + "/data/warps/" + args[0] + ".yml");
+					if (warpFile.exists()){
+						warpConfig.reloadWarpConfig(args[0]);
+						
+						int x = warpConfig.getWarpConfig(args[0]).getInt(args[0] + "." + cworld + ".X");
+						int y = warpConfig.getWarpConfig(args[0]).getInt(args[0] + "." + cworld + ".Y");
+						int z = warpConfig.getWarpConfig(args[0]).getInt(args[0] + "." + cworld + ".Z");
+						
+						Location warp = new Location(Bukkit.getWorld(cworld), x, y, z);
+						player.teleport(warp);
+						
+						player.sendMessage("Warping to: " + args[0]);
+						return true;
+					}else{
+						sender.sendMessage("Warp " + args[0] + " does not exists!");
+						return true;
+					}
 				}
 			}
 		}
