1e862c768c087945244533f4aaa599afcc294658
Stian T. Home <stitofte@yahoo.no>
2013-01-20 00:25:59 +0100

    Trying to fix "warps" command. Not going that well atm.

--- TheCommandBook/src/pro/homiecraft/Commands/tcbWarps.java
+++ TheCommandBook/src/pro/homiecraft/Commands/tcbWarps.java
@@ -9,28 +9,19 @@ import org.bukkit.command.CommandSender;
 import pro.homiecraft.TheCommandBook;
 
 public class tcbWarps implements CommandExecutor {
-	@SuppressWarnings("unused")
 	public boolean onCommand (CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (sender.hasPermission("tcb.Warps")){
 			if(cmd.getName().equalsIgnoreCase("warps")){	
 				String path  = TheCommandBook.pluginST.getDataFolder() + "/data/warps/";
+				File folder = new File(path); 
 				
-				  String files;
-				  File folder = new File(path);
-				  File[] listOfFiles = folder.listFiles(); 
-				 
-				  for (int i = 0; i < listOfFiles.length; i++) 
-				  {
-				 
-				   if (listOfFiles[i].isFile()) 
-				   {
-				   files = listOfFiles[i].getName();
-				   sender.sendMessage(files);
-				   }
-				return true;
+				String[] fileNames = folder.list();
+				String warps = fileNames.toString();
+				String[] warpsS = warps.split(".");
+				
+				sender.sendMessage("Warps: " + warpsS);
 			}
 		}
-	}
 		return false;
-}
+	}
 }
