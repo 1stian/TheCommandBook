585a4a8e647f3b21bc4d667a46b09651006821ed
Stian T. Home <stitofte@yahoo.no>
2013-01-20 02:17:06 +0100

    Fixed warps command! It now lists all warps correctly!

--- TheCommandBook/src/pro/homiecraft/Commands/tcbWarps.java
+++ TheCommandBook/src/pro/homiecraft/Commands/tcbWarps.java
@@ -13,13 +13,28 @@ public class tcbWarps implements CommandExecutor {
 		if (sender.hasPermission("tcb.Warps")){
 			if(cmd.getName().equalsIgnoreCase("warps")){	
 				String path  = TheCommandBook.pluginST.getDataFolder() + "/data/warps/";
-				File folder = new File(path); 
+                File folder = new File(path);
+                String[] fileNames = folder.list();
+                //ArrayList<String> targetNames = new ArrayList<String>();
+                
+                StringBuilder sb = new StringBuilder();
+				for(int i = 0; i < fileNames.length; i++)
+				{
+				    sb.append(fileNames[i]).append(" ");
+				}
+				String msg = sb.toString().trim();
+				sender.sendMessage("Warps: " + msg.replace(".yml", ""));
 				
-				String[] fileNames = folder.list();
-				String warps = fileNames.toString();
-				String[] warpsS = warps.split(".");
-				
-				sender.sendMessage("Warps: " + warpsS);
+//                for (int i = 0; i < fileNames.length; i++) {
+//                  String[] tnt = fileNames[i].split(".");
+//                  
+//                  if(tnt.length == 1){
+//                	targetNames.add(tnt[0]);
+//                    sender.sendMessage("Warps: " + tnt[0]);
+//                    return true;
+//                  }                  
+//                }
+                return true;
 			}
 		}
 		return false;
