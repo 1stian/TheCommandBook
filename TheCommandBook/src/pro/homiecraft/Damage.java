d8d26217dc5f6cce1a7b47ce6d8ae0798d8196b0
Stian T. Home <stitofte@yahoo.no>
2013-01-20 09:53:36 +0100

    Fall damage is now added to god. So that damage will now be canceled.

--- TheCommandBook/src/pro/homiecraft/Damage.java
+++ TheCommandBook/src/pro/homiecraft/Damage.java
@@ -3,9 +3,11 @@ package pro.homiecraft;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
-//import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
 import pro.homiecraft.Commands.Resources.damageMap;
 
 public class Damage implements Listener {
@@ -20,6 +22,13 @@ public class Damage implements Listener {
 			if(event.getDamager() instanceof Monster){
 				event.setCancelled(true);
 			}
+		}
+	}
+	@EventHandler
+	public void onBlockDamage(EntityDamageEvent event){
+		event.getCause();
+		if(event.getCause() == DamageCause.FALL){
+			event.setCancelled(true);
 		}
 	}
 }
